d83a8dfcbc7f0592cf9e37fa287c03acf48b69d8
Will Papper <contact@papper.me>
2024-02-13 15:50:55 -0800

    doc: Add info on Puppeteer config source

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,3 +1,10 @@
+// Found via
+// https://community.render.com/t/error-could-not-found-chromium/9848/7 to get
+// Puppeteer working on Render.
+
+// Note that you'll want to also update the build command on Render to:
+// yarn && yarn run install-chrome
+
 const { join } = require("path");
 
 /**
