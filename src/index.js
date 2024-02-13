61cf17ff7746412c9b6111010adfab6087237099
Will Papper <contact@papper.me>
2024-02-13 15:02:50 -0800

    refactor: Store image generation in memory

--- src/index.js
+++ src/index.js
@@ -16,6 +16,15 @@ app.set("views", __dirname + "/views");
 
 require("dotenv").config();
 
+let browser;
+
+// Close the browser when the process is terminated
+process.on("SIGINT", async () => {
+  console.log("Closing browser");
+  if (browser) await browser.close();
+  process.exit();
+});
+
 // If we receive a get request, we know that this is the initial request to the
 // Frame
 app.get("/", async (req, res) => {
@@ -26,8 +35,14 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/frame-image", async (req, res) => {
-  await generateImage();
-  res.sendFile(__dirname + "/screenshot.png");
+  try {
+    const screenshotBuffer = await generateImage("http://localhost:3000");
+    res.setHeader("Content-Type", "image/png");
+    res.send(screenshotBuffer);
+  } catch (error) {
+    console.error("Error generating screenshot:", error);
+    res.status(500).send("Failed to generate screenshot");
+  }
 });
 
 // If we receive a post request, we know that this is a subsequent request to
@@ -41,15 +56,24 @@ app.get("/healthz", async (req, res) => {
   res.send("ok");
 });
 
+// Start the Express server
 const port = process.env.NODE_ENV === "production" ? 80 : 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
+  startBrowser(); // Launch the browser when the server starts
 });
 
-async function generateImage() {
+// Start the browser for Puppeteer
+async function startBrowser() {
+  browser = await puppeteer.launch();
+}
+
+// Function to generate a screenshot of a given URL and return as a buffer
+async function generateImage(url) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("http://localhost:3000", { waitUntil: "networkidle2" });
-  await page.screenshot({ path: "screenshot.png" });
+  await page.goto(url, { waitUntil: "networkidle2" });
+  const screenshotBuffer = await page.screenshot({ encoding: "binary" });
   await browser.close();
+  return screenshotBuffer;
 }
