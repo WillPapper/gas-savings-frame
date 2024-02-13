7194b9c881ae9b893d9c6beb6a6a86d192361cd9
Will Papper <contact@papper.me>
2024-02-13 14:53:20 -0800

    feat: Add Mustache

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mustacheExpress = require("mustache-express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const { ethers } = require("ethers");
@@ -7,13 +8,20 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+// Register '.mustache' extension with The Mustache Express
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", __dirname + "/views");
+
 require("dotenv").config();
 
 // If we receive a get request, we know that this is the initial request to the
 // Frame
 app.get("/", async (req, res) => {
-  // Return the HTML file
-  res.sendFile(__dirname + "/public/index.html");
+  res.render("index", {
+    title: "Hello, Mustache!",
+    message: "Mustache is working with Express!",
+  });
 });
 
 // If we receive a post request, we know that this is a subsequent request to
